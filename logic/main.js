46eefe24416615a64157ec2b596949e20136828d
rizalkalam <rizalkalam78@gmail.com>
2023-11-14 15:40:35 +0700

    [RK] add login and register

---
+++ logic/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('password');
+    const konfirmasiInput = document.getElementById('konfirmasi');
+    const togglePasswordButton = document.getElementById('togglePassword');
+    const toggleKonfirmasiButton = document.getElementById('toggleKonfirmasi');
+
+    togglePasswordButton.addEventListener('click', function() {
+        // Periksa tipe input saat ini
+        const isPasswordType = passwordInput.type === 'password';
+
+        // Ganti tipe input sesuai dengan status sebelumnya
+        passwordInput.type = isPasswordType ? 'text' : 'password';
+
+        // Ganti gambar mata sesuai dengan status sebelumnya
+        togglePasswordButton.innerHTML = `<img src="../assets/${isPasswordType ? 'eye' : 'eye-slash'}-icon.svg" alt="">`;
+    });
+
+    toggleKonfirmasiButton.addEventListener('click', function() {
+        // Periksa tipe input saat ini
+        const isPasswordType = konfirmasiInput.type === 'password';
+
+        // Ganti tipe input sesuai dengan status sebelumnya
+        konfirmasiInput.type = isPasswordType ? 'text' : 'password';
+
+        // Ganti gambar mata sesuai dengan status sebelumnya
+        toggleKonfirmasiButton.innerHTML = `<img src="../assets/${isPasswordType ? 'eye' : 'eye-slash'}-icon.svg" alt="">`;
+    });
+});
