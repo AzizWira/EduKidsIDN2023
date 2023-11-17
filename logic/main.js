5088fc7f2e4c460c4bf282db24f1234f0bbfc754
rizalkalam <rizalkalam78@gmail.com>
2023-11-17 14:12:13 +0700

    [RK] fix bug login and register page

--- logic/main.js
+++ logic/main.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("password");
+  const passwordRegisterInput = document.getElementById("password-register");
   const konfirmasiInput = document.getElementById("konfirmasi");
   const togglePasswordButton = document.getElementById("togglePassword");
+  const togglePassworRegisterdButton = document.getElementById("togglePasswordRegister");
   const toggleKonfirmasiButton = document.getElementById("toggleKonfirmasi");
 
   togglePasswordButton.addEventListener("click", function () {
@@ -12,7 +14,20 @@ document.addEventListener("DOMContentLoaded", function () {
     passwordInput.type = isPasswordType ? "text" : "password";
 
     // Ganti gambar mata sesuai dengan status sebelumnya
-    togglePasswordButton.innerHTML = `<img src="../assets/${
+    togglePasswordButton.innerHTML = `<img src="assets/${
+      isPasswordType ? "eye" : "eye-slash"
+    }-icon.svg" alt="">`;
+  });
+
+  togglePassworRegisterdButton.addEventListener("click", function () {
+    // Periksa tipe input saat ini
+    const isPasswordType = passwordRegisterInput.type === "password";
+
+    // Ganti tipe input sesuai dengan status sebelumnya
+    passwordRegisterInput.type = isPasswordType ? "text" : "password";
+
+    // Ganti gambar mata sesuai dengan status sebelumnya
+    togglePassworRegisterdButton.innerHTML = `<img src="assets/${
       isPasswordType ? "eye" : "eye-slash"
     }-icon.svg" alt="">`;
   });
